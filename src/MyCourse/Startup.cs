4267bf90f748926739ccaadccb892548dcfbb2d9
Alessio Lodolo <alessio.lodolo@gmail.com>
2020-06-14 17:46:51 +0200

    Aggiunto asp.net core mvc

--- src/MyCourse/Startup.cs
+++ src/MyCourse/Startup.cs
@@ -9,29 +9,32 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace MyCourse
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+   public class Startup
+   {
+      // This method gets called by the runtime. Use this method to add services to the container.
+      // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+      public void ConfigureServices(IServiceCollection services)
+      {
+         //importante per il funzionamento del middleware del metodo Configure
+         services.AddMvc();
+      }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            //if (env.IsDevelopment())
-            if (env.IsEnvironment("Development"))
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            app.UseStaticFiles();
-            
-            app.Run(async (context) =>
-            {
-                string nome = context.Request.Query["nome"];
-                await context.Response.WriteAsync($"Hello {nome}!");
-            });
-        }
-    }
+      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+      {
+         //if (env.IsDevelopment())
+         if (env.IsEnvironment("Development"))
+         {
+            app.UseDeveloperExceptionPage();
+         }
+         app.UseStaticFiles();
+
+         //aggiungo middleware di routing mvc
+         //app.UseMvcWithDefaultRoute(); sotto in modo esplicito e piu modulare
+         app.UseMvc(routeBuilder =>
+         {
+            routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+         });
+      }
+   }
 }
